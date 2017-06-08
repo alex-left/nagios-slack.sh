96bb648b21f69c368f7e5abd95b0d0a5797c337f
Alex <alejandro.izquierdo.b@gmail.com>
2017-06-08 12:59:18 +0200

    Create nagios-slack.sh

--- nagios-slack.sh
+++ nagios-slack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-HOOK='https://hooks.slack.com/services/some-hook-url'
-TEXT=payload="{\"text\": \"${2} ALERT: \nDATE: ${7} \nHOST: ${1} ( ${6} ) \nSTATE: ${4} \nSERVICE: ${3} \nMESSAGE: ${5} <https://mynagios.com/cgi-bin/status.cgi?host=${1} |See me on Nagios>\"}"
+HOOK='https://hooks.slack.com/some-url-hook'
+TEXT=payload="{\"text\": \"-------------\n${2} ALERT: \nDATE: ${7} \nHOST: ${1} ( ${6} ) \nSTATE: ${4} \nSERVICE: ${3} \nMESSAGE: ${5} <https://mynagios-url.com/cgi-bin/status.cgi?host=${1} |See me on Nagios>\n-------------\"}"
 curl -X POST --data-urlencode "$TEXT" "${HOOK}"
